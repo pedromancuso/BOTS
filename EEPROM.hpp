f81e181d8b83c2cdf63fbb8a4825fae825b51c85
pedromancuso <pedromancuso@gmail.com>
2020-08-12 02:28:21 -0300

    Correccion de Despliegue HTML

---
+++ EEPROM.hpp
@@ -0,0 +1,16 @@
+#define DATA_OFFSET 10
+EEPROM_Rotate EEPROMr;
+////////////////////////////////EEPROMSETUP//////////////////////////////////////////////////////////////////////////
+void eepromsetup(){EEPROMr.size(4);EEPROMr.begin(512);Serial.println("EEPROM INICIADA 512 Kb");}
+////////////////////////////////WRITESTRING//////////////////////////////////////////////////////////////////////////
+void writeString(String str, int pos){
+  for (int i=0; i<str.length(); i++){int s = str[i];EEPROMr.write(i+pos+DATA_OFFSET, s);}
+  EEPROMr.write(str.length()+pos+DATA_OFFSET,'\0');
+  EEPROMr.commit();
+}
+////////////////////////////////READCHAR//////////////////////////////////////////////////////////////////////////
+char * readChar(int pos){
+  char * data=new char[100];int len = 0;unsigned char k;
+  while(k != '\0' && len <= 100){k=char(EEPROMr.read(len+pos+DATA_OFFSET));data[len]=k;len++;}
+  return data;
+}
