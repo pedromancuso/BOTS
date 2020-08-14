64e109585e88ebf1918315d71d3a21837eff9969
pedromancuso <pedromancuso@gmail.com>
2020-08-14 17:28:46 -0300

    CSS Corregido

--- botsjs.js
+++ botsjs.js
@@ -29,22 +29,87 @@ document.addEventListener('click',function(e){
 		}
 	for (var i = 1; i <= 7; i++) {if(e.target && e.target.id== ('D'+i)){selectday('D'+i);}}
 });
+function myajax(stringpost,url){
+	return new Promise(resolve => {
+		setTimeout(() => {
+			var xmlHttp = new XMLHttpRequest();
+			xmlHttp.onreadystatechange=function() {
+				if (xmlHttp.readyState === 4 && xmlHttp.status === 200) {
+					resolve(xmlHttp.responseXML.getElementById('respxml').childNodes[0].nodeValue);
+				}
+			};
+			xmlHttp.open('POST',url,true);
+			xmlHttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+			xmlHttp.send(stringpost);
+		}, 2000);
+	});
+}
+var max=0;var res=[];
+async function mqtt(){
+	res=[];
+	var message = await myajax(null,'/htmlclientesxml');
+	ID('clientes').innerHTML='';
+	var progx = message.split('$');
+	if (progx.length>1) {
+		for (var i = 0; i < progx.length-1; i++) {
+			res[i] = progx[i].split('?');if (res[i][0]>max) {max=res[i][0];}
+			ID('clientes').innerHTML+=
+			`<tr><td class='option'>`+res[i][1]+`</td><td style='text-align: right;'><label id='label`+res[i][0]+`' class='switch'><input id='check`+res[i][0]+`' type='checkbox' class='optioninput'><span class='slider round'></span></label><span id='estado`+res[i][0]+`' class='estado'></span></td></tr>`;
+	}
+	}else{ID('programasclientes').innerHTML+=`<tr style='cursor:pointer'><td id='nodev' class='mytd'>NO DEVS</td></tr>`;}
+	programasclientes();
+}
+var programasres=[];
+async function programasclientes(){	
+	programasres=[];
+	var message=await myajax(null,'/htmlprogramasclientesxml');
+	ID('programasclientes').innerHTML='';
+	ID('devicesprogram').innerHTML='';
+	var progx=message.split('$');
+	if (progx.length>1){
+		for (var i=0;i<progx.length-1;i++) {
+			programasres[i]=progx[i].split('?');
+			if (programasres[i][0]>max) {max=programasres[i][0];}
+			ID('programasclientes').innerHTML+=
+			`<tr id='programa`+programasres[i][0]+`' style='cursor:pointer;'><td class='deleteop mytd' id='deleteprogram`+programasres[i][0]+`' style='display: none;'><input type='checkbox' id='del`+programasres[i][0]+`' checked data-toggle='toggle'><label for='del`+programasres[i][0]+`'></label></td><td class='mytd' id='nombreprograma`+programasres[i][0]+`' style='width: 100%;'>`+programasres[i][1]+`</td><td class='mytd' style='text-align: right;width: 25%'><label class='switch' style='display:block'><input type='checkbox' id='programaestado`+programasres[i][0]+`' class='optioninput'><span class='slider round'></span></label></td></tr>`;					
+		}
+		for (var i=0;i<programasres.length-1;i++){
+			var u= ID('programaestado'+programasres[i][0]);///aca esta el problema
+			if (programasres[i][2]==1){u.checked=true;}else if(programasres[i][2]==0){u.checked=false;}
+		}
+		
+	}else{
+		ID('programasclientes').innerHTML+=`<tr style='cursor:pointer'><td id='agregarprograma' class='mytd'>+ Agregar programa</td></tr>`;
+	}
+	for (var j=0;j<res.length;j++){
+		ID('devicesprogram').innerHTML+=`<tr><td class='mytd'>`+res[j][1]+`</td><td class='mytd' style='text-align: right;'><input type='checkbox' id='devicesprogram`+res[j][0]+`'><label for='devicesprogram`+res[j][0]+`'></label></td></tr>`;
+	}
+	setTimeout(receive,1000);
+}
 function recutoggle(){
 	var x1 = ID('recucontramod');var x2 = ID('botsloginform');
 	if (x1.style.display=='block') {x1.style.display='none';x2.style.display='block';}else {x1.style.display='block';x2.style.display='none';}
 }
 function selectday(a){
 	ID(a).value=!ID(a).value;
-	if (ID(a).value==false){ID(a).className = 'button special fit small disabled';}
-	if (ID(a).value==true){ID(a).className = 'button special fit small';}
+	if (ID(a).value==false){ID(a).className = 'day special disabled';}
+	if (ID(a).value==true){ID(a).className = 'day special';}
 }
 function delmenu() {
 	var cols = document.getElementsByClassName('deleteop');
 	for(i = 0; i < cols.length; i++) {
-		if (cols[i].style.display=='none'){cols[i].style.display='table-cell';ID('eliminarprogramas').style.display='table-cell';}
+		if (cols[i].style.display=='none'){cols[i].style.display='inline-block';ID('eliminarprogramas').style.display='table-cell';}
 		else{cols[i].style.display='none';ID('eliminarprogramas').style.display='none';}
 	}
+	j1();
 }
+function j1(){
+var x1=document.getElementById('1');var x3=document.getElementById('3');
+var x2=document.getElementById('2');var x4=document.getElementById('4');
+if (x1.style.display=='none') {x2.style.display='inline-block';x1.style.display='none';x3.style.display='none';x4.style.display='inline-block';}
+else{x2.style.display='none';x1.style.display='inline-block';x3.style.display='inline-block';x4.style.display='none';}
+}
+
 function dispositivos(s){
 	ID('PROGRAMASCONTAINER').className = 'container';
 	ID('programas').style.display='inline-block';
@@ -62,7 +127,7 @@ function dispositivos(s){
 		ID('CLIENTESCONTAINER').style.display = 'none';
 		ID('PROGRAMASCONTAINER').style.display = 'block';
 		var cols = document.getElementsByClassName('deleteop');for(i = 0; i < cols.length; i++) {cols[i].style.display='none';}
-		for (var i = 0; i <= programasres.length; i++) {
+		for (var i = 0; i <= programasres.length-1; i++) {
 			var u= ID('programaestado'+programasres[i][0]);
 			if (programasres[i][2]==1) {u.checked=true;} else if(programasres[i][2]==0) {u.checked=false;}
 		}
@@ -73,11 +138,9 @@ function programeditshow(IDx){
 		ID('programid').value='0';
 		ID('editarprogramatitulo').innerHTML='NUEVO PROGRAMA';
 		ID('nombreprogramaedit').value='';
-		for (var i = 1; i <= 7; i++) {ID('D'+i).value=false;ID('D'+i).className='button special fit small disabled';}
+		for (var i = 1; i <= 7; i++) {ID('D'+i).value=false;ID('D'+i).className='day special disabled';}
 		ID('HORAINICIO').value='HH';
-		ID('MINUTOINICIO').value='MM';
 		ID('HORAFIN').value='HH';
-		ID('MINUTOFIN').value='MM';
 		for (var i = 0; i < res.length; i++) {ID('devicesprogram'+res[i][0]).checked=false;}
 	}else{
 		ID('editarprogramatitulo').innerHTML='MODIFICAR PROGRAMA';
@@ -89,10 +152,8 @@ function programeditshow(IDx){
 				for (var d = 0; d <= days.length-1; d++) {if(days[d]!='D' && days[d]!=''){selectday('D'+days[d]);}}
 				var inicio = programasres[i][4].split(':');
 				ID('HORAINICIO').value=inicio[0];
-				ID('MINUTOINICIO').value=inicio[1];
 				var fin = programasres[i][5].split(':');
 				ID('HORAFIN').value=fin[0];
-				ID('MINUTOFIN').value=fin[1];
 				var dev =programasres[i][6].split('%');
 				for (var v = 0; v < dev.length; v++) {if(dev[v]!='%' && dev[v]!=''){ID('devicesprogram'+dev[v]).checked=true;}}
 			}
@@ -100,24 +161,9 @@ function programeditshow(IDx){
 	}
 	if(ID('programas').style.display==='none'){ID('programas').style.display='inline-block';ID('modificacionprograma').style.display='none';ID('main').className ='wrapper';}
 	else{ID('programas').style.display='none';ID('modificacionprograma').style.display='inline-block';ID('main').className='wrapper border';}
-	ID('tabladevices').style.height=(((ID('programbutton').getBoundingClientRect().top)-(ID('HMF').getBoundingClientRect().bottom))*0.95)+'px';
+	ID('tabladevices').style.height=(((ID('programbutton').getBoundingClientRect().top)-(ID('titulotabla').getBoundingClientRect().bottom)))+'px';
 }
 function check(idx){if (ID('check'+idx).checked===true){send(idx+'I');}else{send(idx+'O');}}
-function myajax(stringpost,url){
-	return new Promise(resolve => {
-		setTimeout(() => {
-			var xmlHttp = new XMLHttpRequest();
-			xmlHttp.onreadystatechange=function() {
-				if (xmlHttp.readyState === 4 && xmlHttp.status === 200) {
-					resolve(xmlHttp.responseXML.getElementById('respxml').childNodes[0].nodeValue);
-				}
-			};
-			xmlHttp.open('POST',url,true);
-			xmlHttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-			xmlHttp.send(stringpost);
-		}, 2000);
-	});
-}
 async function wificonfigxml() {
 	var msg = await myajax('STAssid:'+ID('STAssid').value+'?STApassword:'+ID('STApassword').value+'?','/wificonfigxml');
 	if (msg=='1') {window.location.href = '/botslogin';}
@@ -160,48 +206,6 @@ async function eliminarprogramas(){
 }
 async function modificarestadonprograma(u){await myajax({ID:u,ESTADO:ID('programaestado'+u).checked},'/htmlmodificarestadoprogramaxml');programasclientes();}
 async function send(dato){await myajax('strtopic:'+dato+'?','/htmlsendxml');}
-var max=0;var res=[];
-async function mqtt(){
-	res=[];
-	var message = await myajax(null,'/htmlclientesxml');
-	ID('clientes').innerHTML='';
-	var progx = message.split('$');
-	if (progx.length>1) {
-		for (var i = 0; i < progx.length-1; i++) {
-			res[i] = progx[i].split('?');if (res[i][0]>max) {max=res[i][0];}
-			ID('clientes').innerHTML+=
-			`<tr><td class='option'>`+res[i][1]+`</td><td style='text-align: right;'><label id='label`+res[i][0]+`' class='switch'><input id='check`+res[i][0]+`' type='checkbox' class='optioninput'><span class='slider round'></span></label><span id='estado`+res[i][0]+`' class='estado'></span></td></tr>`;
-	}
-	}else{ID('programasclientes').innerHTML+=`<tr style='cursor:pointer'><td id='nodev' class='mytd'>NO DEVS</td></tr>`;}
-	programasclientes();
-}
-var programasres=[];
-async function programasclientes(){	
-	programasres=[];
-	var message=await myajax(null,'/htmlprogramasclientesxml');
-	ID('programasclientes').innerHTML='';
-	ID('devicesprogram').innerHTML='';
-	var progx=message.split('$');
-	if (progx.length>1){
-		for (var i=0;i<progx.length-1;i++) {
-			programasres[i]=progx[i].split('?');
-			if (programasres[i][0]>max) {max=programasres[i][0];}
-			ID('programasclientes').innerHTML+=
-			`<tr id='programa`+programasres[i][0]+`' style='cursor:pointer;'><td class='deleteop mytd' id='deleteprogram`+programasres[i][0]+`' style='display: none;'><input type='checkbox' id='del`+programasres[i][0]+`' checked data-toggle='toggle'><label for='del`+programasres[i][0]+`'></label></td><td class='mytd' id='nombreprograma`+programasres[i][0]+`' style='width: 100%;'>`+programasres[i][1]+`</td><td class='mytd' style='text-align: right;width: 25%'><label class='switch' style='display:block'><input type='checkbox' id='programaestado`+programasres[i][0]+`' class='optioninput'><span class='slider round'></span></label></td></tr>`;					
-		}
-		for (var i=0;i<programasres.length;i++){
-			var u= ID('programaestado'+programasres[i][0]);
-			if (programasres[i][2]==1){u.checked=true;}else if(programasres[i][2]==0){u.checked=false;}
-		}
-		
-	}else{
-		ID('programasclientes').innerHTML+=`<tr style='cursor:pointer'><td id='agregarprograma' class='mytd'>+ Agregar programa</td></tr>`;
-	}
-	for (var j=0;j<res.length;j++){
-		ID('devicesprogram').innerHTML+=`<tr><td class='mytd'>`+res[j][1]+`</td><td class='mytd' style='text-align: right;'><input type='checkbox' id='devicesprogram`+res[j][0]+`'><label for='devicesprogram`+res[j][0]+`'></label></td></tr>`;
-	}
-	setTimeout(receive,1000);
-}
 async function nuevoprograma(){
 	var programid=ID('programid').value;
 	var nombreprograma=ID('nombreprogramaedit').value;
@@ -229,14 +233,9 @@ async function nuevoprograma(){
 }
 function user(){
 	var HeaderHeight=ID('header').getBoundingClientRect().height;
-	var footerHeight=ID('footerx').getBoundingClientRect().height;
+	var footerHeight=ID('footeruser').getBoundingClientRect().height;
 	var mainHeight=document.body.clientHeight-HeaderHeight-footerHeight;
 	ID('main').style.top=HeaderHeight+'px';ID('main').style.bottom=footerHeight+'px';
-	for (var i = 0; i <= 60; i++) {
-		var line =`<option value='`+i+`'>`+i+`</option>`;
-		if (i<=24) {ID('HORAINICIO').innerHTML+=line;ID('HORAFIN').innerHTML+=line;}
-		ID('MINUTOINICIO').innerHTML+=line;ID('MINUTOFIN').innerHTML+=line;
-	}
 	/*mqtt();*/
 }
 if(document.body.contains(ID('webuser'))){console.log('webuser');user();}
