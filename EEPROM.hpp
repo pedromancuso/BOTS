8e7029de0606911d0f39fcd31945b5b93ff12cf3
pedromancuso <pedromancuso@gmail.com>
2020-11-15 16:09:47 -0300

    BOTS7
    Version7.NoTocar.

--- EEPROM.hpp
+++ EEPROM.hpp
@@ -1,16 +1,25 @@
 #define DATA_OFFSET 10
 EEPROM_Rotate EEPROMr;
 ////////////////////////////////EEPROMSETUP//////////////////////////////////////////////////////////////////////////
-void eepromsetup(){EEPROMr.size(4);EEPROMr.begin(512);Serial.println("EEPROM INICIADA 512 Kb");}
+void eepromsetup(){EEPROMr.size(4);EEPROMr.begin(512);}
 ////////////////////////////////WRITESTRING//////////////////////////////////////////////////////////////////////////
 void writeString(String str, int pos){
-  for (int i=0; i<str.length(); i++){int s = str[i];EEPROMr.write(i+pos+DATA_OFFSET, s);}
+  for (int i=0; i<str.length(); i++){
+    int s = str[i];
+    EEPROMr.write(i+pos+DATA_OFFSET, s);
+  }
   EEPROMr.write(str.length()+pos+DATA_OFFSET,'\0');
   EEPROMr.commit();
 }
-////////////////////////////////READCHAR//////////////////////////////////////////////////////////////////////////
-char * readChar(int pos){
-  char * data=new char[100];int len = 0;unsigned char k;
-  while(k != '\0' && len <= 100){k=char(EEPROMr.read(len+pos+DATA_OFFSET));data[len]=k;len++;}
-  return data;
+////////////////////////////////READSTRING//////////////////////////////////////////////////////////////////////////
+String readString(int pos){
+  char data[100]; // Max 100 Bytes
+  int len = 0;
+  unsigned char k;
+  while(k != '\0' && len <= 100){
+    k = char(EEPROMr.read(len+pos+DATA_OFFSET));
+    data[len] = k;
+    len++;
+  }
+  return String(data);
 }
